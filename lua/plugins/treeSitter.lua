ad4caf1ab1c5836c549546edc822257285a5108b
chandanyuva <chandanyuva1511@gmail.com>
2025-04-26 23:26:31 +0530

    added treesitter

---
+++ lua/plugins/treeSitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html" },
+      auto_install = true,
+      hightlight = {
+	enable=true,
+      },
+      indent = {
+	enable=true,
+      },
+      autotage = {
+	enable=true,
+      },
+    })
+  end
+}
