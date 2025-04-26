cf333a036db53341372b74430d5dbcf7aa62e784
chandanyuva <chandanyuva1511@gmail.com>
2025-04-27 00:29:50 +0530

    made changes to keybinds and options

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>cd",vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Alt Up/Down in vscode
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Alt Up/Down in vscode
+
+vim.keymap.set("n", "J", "mzJ`z") -- Remap joining lines
+
+-- reload without exiting vim
+vim.keymap.set("n", "<leader>rl", "<cmd>source ~/.config/nvim/init.lua<cr>")
+
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,34 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
+
+-- indentation and tabs
+vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+
+-- keep cursor at least 8 rows from top/bot
+vim.opt.scrolloff = 8
+
+-- split windows
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- appearance
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+
+-- faster cursor hold
+vim.opt.updatetime = 50
+
+-- undo dir settings
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
