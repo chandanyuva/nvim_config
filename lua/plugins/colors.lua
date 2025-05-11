24c393fbee1a6f683c71ed63acbfba93afffe914
yuva-arch-laptop <chandanyuva1511@gmail.com>
2025-05-11 11:29:47 +0530

    added tmf.nvim
    uses yazi(or any other) inside a terminal window inside nvim

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,15 +1,17 @@
 local function enable_transparency()
-  vim.api.nvim_set_hl(0,"Normal",{bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
+
+
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        vim.cmd.colorscheme "tokyonight-night"
-	enable_transparency()
-    end,
-  },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd.colorscheme "tokyonight-night"
+            enable_transparency()
+        end,
+    },
 }
---
+++ lua/plugins/tmf.lua
@@ -0,0 +1,28 @@
+return {
+    "rolv-apneseth/tfm.nvim",
+    lazy = false,
+    opts = {
+        file_manager = "yazi",
+        replace_netrw = true,
+        enable_cmds = true,
+        keybindings = {
+            ["<ESC>"] = "q",
+            ["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
+            ["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
+            ["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
+        },
+        ui = {
+            border = "rounded",
+            height = 0.9,
+            width = 0.9,
+            x = 0.5,
+            y = 0.5,
+        },
+    },
+    keys = {
+        { "<leader>e", "<cmd>Tfm<CR>", desc = "TFM" },
+        -- { "<leader>mh", "<cmd>TfmSplit<CR>",   desc = "TFM - horizontal split" },
+        -- { "<leader>mv", "<cmd>TfmVsplit<CR>",  desc = "TFM - vertical split" },
+        -- { "<leader>mt", "<cmd>TfmTabedit<CR>", desc = "TFM - new tab" },
+    },
+}
