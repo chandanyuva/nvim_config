71438bb9c80e5f3c42674f297b56e88ea3b9db74
chandan msi-wsl <chandanyuva1511@gmail.com>
2025-05-17 14:58:05 +0530

    added some extra plugins from kickstart extras
    also disabled transparency in themes

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,8 +1,32 @@
 return {
-    { -- Show CSS Colors
-        'brenoprata10/nvim-highlight-colors',
-        config = function()
-            require('nvim-highlight-colors').setup({})
-        end
-    },
+	{
+		-- Show CSS Colors
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
+	{
+		-- Highlight todo, notes, etc in comments
+		{
+			"folke/todo-comments.nvim",
+			event = "VimEnter",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			opts = { signs = false },
+		},
+	},
+	{
+		-- autopairs
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+	},
 }
--- lua/plugins/treeSitter.lua
+++ lua/plugins/treeSitter.lua
@@ -1,20 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html" },
-      auto_install = true,
-      hightlight = {
-	enable=true,
-      },
-      indent = {
-	enable=true,
-      },
-      autotage = {
-	enable=true,
-      },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "bash", "diff", "c", "lua", "luadoc", "markdown", "vim", "vimdoc", "javascript", "html" },
+			auto_install = true,
+			hightlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			autotage = {
+				enable = true,
+			},
+		})
+	end,
 }
