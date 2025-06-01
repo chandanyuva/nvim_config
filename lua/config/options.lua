a8606bf7ceed243c9022ad466c295414a1317673
archLaptop <chandanyuva1511@gmail.com>
2025-06-01 18:00:16 +0530

    added opts from nvim base config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.cursorline = true
@@ -32,26 +32,69 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-
 -- from kickstart
 
-vim.o.mouse = 'a' -- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
 vim.g.have_nerd_font = true
-
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 
 -- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- from nvim base config
+
+vim.wo.number = true -- Make line numbers default (default: false)
+vim.o.relativenumber = true -- Set relative numbered lines (default: false)
+vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
+vim.o.wrap = true -- Display lines as one long line (default: true)
+vim.o.linebreak = true -- Companion to wrap, don't split words (default: false)
+vim.o.mouse = "a" -- Enable mouse mode (default: '')
+vim.o.autoindent = true -- Copy indent from current line when starting new one (default: true)
+vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+vim.o.smartcase = true -- Smart case (default: false)
+vim.o.shiftwidth = 2 -- The number of spaces inserted for each indentation (default: 8)
+vim.o.tabstop = 2 -- Insert n spaces for a tab (default: 8)
+vim.o.softtabstop = 2 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
+vim.o.expandtab = true -- Convert tabs to spaces (default: false)
+vim.o.scrolloff = 15 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
+vim.o.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
+vim.o.cursorline = true -- [c] Highlight the current line (default: false)
+vim.o.splitbelow = true -- Force all horizontal splits to go below current window (default: false)
+vim.o.splitright = true -- Force all vertical splits to go to the right of current window (default: false)
+vim.o.hlsearch = false -- Set highlight on search (default: true)
+vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
+vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+-- vim.o.whichwrap = 'bs<>[]hl' -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
+vim.o.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
+vim.o.swapfile = false -- Creates a swapfile (default: true)
+vim.o.smartindent = true -- Make indenting smarter again (default: false)
+vim.o.showtabline = 2 -- Always show tabs (default: 1)
+vim.o.backspace = "indent,eol,start" -- Allow backspace on (default: 'indent,eol,start')
+vim.o.pumheight = 10 -- Pop up menu height (default: 0)
+vim.o.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
+vim.wo.signcolumn = "yes" -- Keep signcolumn on by default (default: 'auto')
+vim.o.fileencoding = "utf-8" -- The encoding written to a file (default: 'utf-8')
+vim.o.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
+vim.o.breakindent = true -- Enable break indent (default: false)
+vim.o.updatetime = 250 -- Decrease update time (default: 4000)
+vim.o.timeoutlen = 700 -- Time to wait for a mapped sequence to complete (in milliseconds) (default: 1000)
+vim.o.backup = false -- Creates a backup file (default: false)
+vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
+vim.o.undofile = true -- Save undo history (default: false)
+vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: 'menu,preview')
+vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
